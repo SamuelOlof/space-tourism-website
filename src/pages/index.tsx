bb1558dd13ea1b983486c850dd794545ce4153fc
SamuelOlof <samuelolofintoye@gmail.com>
2023-04-07 10:49:08 +0100

    Space Tourism Website

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Crew from "./crew/Crew";
+import Destination from "./destination/Destination";
+import Home from "./home/Home";
+import Technology from "./technology/Technology";
+
+export{
+    Home,
+    Destination,
+    Crew,
+    Technology
+}
