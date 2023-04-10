295f8eb3cc4aaf2dbe3aafbb40c429d315d8ad5b
SamuelOlof <samuelolofintoye@gmail.com>
2023-04-10 13:36:18 +0100

    Responsive navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,84 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import  React, { useState } from 'react';
+// import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
-import './App.css';
-import { Home, Crew, Destination, Technology  } from './pages';
+import './App.scss';
+import { Menu } from './components';
+import { images } from './constants';
 
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/destination" element={<Destination />} />
-      <Route path="/crew" element={<Crew />} />
-      <Route path="/technology" element={<Technology />} />
-    </Routes>
+    <>
+      <nav className='app__menu'>
+        <div className='app__menu-logo'>
+          <img src={images.logo} alt="Space tourism logo" />
+        </div>
+
+        <div className='app__menu-hamburger'>
+          <img src={images.iconHamburger} alt="" onClick={() => setToggle(true)} />
+        </div>
+
+        {toggle &&(
+            <motion.div 
+                whileInView = {{ x: [300,0]}}
+                transition = {{duration: 0.85, ease: 'ease-out'}}
+            >
+              <div className='app__menu-container'>
+                <div className='app__menu-close'>
+                  <img src={images.iconClose} alt="" onClick={() => setToggle(false)} />
+                </div>
+
+                <ul className='app__menu-links'>
+                  <li>
+                    <Link className='link' to="/" onClick={() => setToggle(false)}><span>00  </span> Home </Link>
+                  </li>
+
+                  <li>
+                    <Link className='link' to="/destination" onClick={() => setToggle(false) }><span>01  </span> Destination </Link>
+                  </li>
+
+                  <li>
+                    <Link className='link' to="/crew" onClick={() => setToggle(false) }><span>02 </span> Crew </Link>
+                  </li>
+
+                  <li>
+                    <Link className='link' to="/technology" onClick={() => setToggle(false) }><span>03 </span> Technology </Link>
+                  </li>
+                </ul>   
+              </div>  
+            </motion.div> 
+        )}
+
+        <div className='app__menu-navbar-container'>
+          <ul className='app__menu-navbar-links'>
+            <li>
+              <Link className='link' to="/"> Home </Link>
+            </li>
+
+            <li>
+              <Link className='link' to="/destination"> Destination </Link>
+            </li>
+
+            <li>
+              <Link className='link' to="/crew"> Crew </Link>
+            </li>
+
+            <li>
+              <Link className='link' to="/technology"> Technology </Link>
+            </li>
+          </ul>
+        </div>
+
+      </nav>
+
+      
+      <Menu/>
+    </>
+
   );
 }
 
---
+++ src/constants/images.tsx
@@ -0,0 +1,67 @@
+import logo from '../assets/shared/logo.svg';
+import iconHamburger from '../assets/shared/icon-hamburger.svg';
+import iconClose from '../assets/shared/icon-close.svg';
+
+import bgHomeDesktop from '../assets/home/background-home-desktop.jpg';
+import bgHomeTablet from '../assets/home/background-home-tablet.jpg';
+import bgHomeMobile from '../assets/home/background-home-mobile.jpg';
+
+import bgDestinationDesktop from '../assets/destination/background-destination-desktop.jpg';
+import bgDestinationTablet from '../assets/destination/background-destination-tablet.jpg';
+import bgDestinationMobile from '../assets/destination/background-destination-mobile.jpg';
+import imageEuropa from '../assets/destination/image-europa.png';
+import imageMars from '../assets/destination/image-mars.png';
+import imageMoon from '../assets/destination/image-moon.png';
+import imageTitan from '../assets/destination/image-titan.png';
+
+import bgCrewDesktop from '../assets/crew/background-crew-desktop.jpg';
+import bgCrewTablet from '../assets/crew/background-crew-tablet.jpg';
+import bgCrewMobile from '../assets/crew/background-crew-mobile.jpg';
+import imageAnousheh from '../assets/crew/image-anousheh-ansari.png';
+import imageDouglas from '../assets/crew/image-douglas-hurley.png';
+import imageMark from '../assets/crew/image-mark-shuttleworth.png';
+import imageVictor from '../assets/crew/image-victor-glover.png';
+
+import bgTechnologyDesktop from '../assets/technology/background-technology-desktop.jpg';
+import bgTechnologyTablet from '../assets/technology/background-technology-tablet.jpg';
+import bgTechnologyMobile from '../assets/technology/background-technology-mobile.jpg';
+import imageLaunchLandscape from '../assets/technology/image-launch-vehicle-landscape.jpg';
+import imageLaunchPortrait from '../assets/technology/image-launch-vehicle-portrait.jpg';
+import imageSpaceLandscape from '../assets/technology/image-space-capsule-landscape.jpg';
+import imageSpacePortrait from '../assets/technology/image-space-capsule-portrait.jpg';
+import imageSpaceportLandscape from '../assets/technology/image-spaceport-landscape.jpg';
+import imageSpaceportPortrait from '../assets/technology/image-spaceport-portrait.jpg';
+
+
+
+export default {
+    logo,
+    iconHamburger,
+    iconClose,
+    bgHomeDesktop,
+    bgHomeTablet,
+    bgHomeMobile,
+    bgDestinationDesktop,
+    bgDestinationTablet,
+    bgDestinationMobile,
+    imageEuropa,
+    imageMars,
+    imageMoon,
+    imageTitan,
+    bgCrewDesktop,
+    bgCrewTablet,
+    bgCrewMobile,
+    imageAnousheh,
+    imageDouglas,
+    imageMark,
+    imageVictor,
+    bgTechnologyDesktop,
+    bgTechnologyTablet,
+    bgTechnologyMobile,
+    imageLaunchLandscape,
+    imageLaunchPortrait,
+    imageSpaceLandscape,
+    imageSpacePortrait,
+    imageSpaceportLandscape,
+    imageSpaceportPortrait
+}
